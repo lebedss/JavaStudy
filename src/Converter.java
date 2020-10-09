b062d228d2703857e3d47612054985402492a0b9
lebedss <lebedevss88@gmail.com>
2020-10-09 07:57:02 +0300

    Конвертер валюты [#381781]

---
+++ src/Converter.java
@@ -0,0 +1,15 @@
+public class Converter {
+
+    public static int rubleToEuro(int value) {
+        return value / 70;
+    }
+    public static int rubleToDollar(int value ) {
+        return value / 60;
+    }
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(180);
+        System.out.println(  "140 rubles are " + euro + " euro.");
+        System.out.println("180 rubles are " + dollar + " dollars.");
+    }
+}
