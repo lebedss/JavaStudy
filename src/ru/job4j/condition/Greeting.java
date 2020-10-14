341644ea83ad260cf4a1f7c5839e5bb82b335c44
lebedss <lebedevss88@gmail.com>
2020-10-14 20:16:02 +0300

    String

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+     String idea ="I like Java!";
+     int year = 2020;
+
+        String result= idea + " But I am a newbie "  +  year;
+        System.out.println(result);
+    }
+}
