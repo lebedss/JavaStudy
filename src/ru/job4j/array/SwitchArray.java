a20f71b5d35f7b0a8a5197bb1ad78a366d63cecc
lebedss <lebedevss88@gmail.com>
2020-11-11 20:04:06 +0300

    Выход за границу массива

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -1,12 +1,11 @@
 package ru.job4j.array;
-
 public class SwitchArray {
     public static int[] swapBorder(int[] array) {
-        int temp = array[0];
-        array[0] = array[5];
-        array[5] = temp;
-        return array;
-    }
+            int temp = array[0];
+            array[0] = array[array.length - 1];
+            array[array.length - 1] = temp;
+            return array;
+        }
 
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5, 6};
