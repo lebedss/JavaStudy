51c5e2cadcafa71f84b18cc7e563a9bf4ac7685f
lebedss <lebedevss88@gmail.com>
2020-11-23 22:00:27 +0300

    Сортировка выборкой. [#381757]

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            /* минимальный элемент в массиве*/
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            /* индекс минимального элемента*/
+            int tmp = data[index];
+            data[index] = data[i];
+            data[i] = tmp;
+        }
+        return data;
+    }
+}
